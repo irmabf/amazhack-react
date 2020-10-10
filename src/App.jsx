ca6f6b00a92b9c95787ee4e293923f54c1842d09
Irma Blanco <irblancofdz@gmail.com>
2020-10-10 14:00:50 +0200

    feat: Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import './App.css';
-import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
-import Login from './components/login/Login';
-import ProductList from './components/product-list/ProductList';
+import "./App.css";
+import React, { useState } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Login from "./components/login/Login";
+import ProductList from "./components/product-list/ProductList";
+import AuthenticatedRoute from "./components/authenticatedroute/AuthenticatedRoute";
 
 function App() {
+  const [user, setUser] = useState();
   return (
     <div className="App">
       {/* We want a header that will appear in every screen */}
       <Switch>
+        <AuthenticatedRoute
+          exact
+          path="/products"
+          user={user}
+          component={(props) => <ProductList {...props} user={user} />}
+        />
         <Route path="/products" component={ProductList} />{" "}
         {/* We don't wan't non-authenticated users to access the previous route! */}
-        <Route path="/" component={Login} />
+        <Route
+          path="/login"
+          component={(props) => (
+            <Login {...props} user={user} setUser={setUser} />
+          )}
+        />
         <Redirect to="/products" />
       </Switch>
     </div>
